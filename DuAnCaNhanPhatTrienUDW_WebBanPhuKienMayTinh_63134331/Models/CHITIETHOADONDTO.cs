0ba5e1fca99754ce5ff68ff65822c54c56bb86f8
LBKhoa <lebaokhoa.200329@gmail.com>
2024-11-05 19:05:54 +0700

    update

---
+++ DuAnCaNhanPhatTrienUDW_WebBanPhuKienMayTinh_63134331/Models/CHITIETHOADONDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuAnCaNhanPhatTrienUDW_WebBanPhuKienMayTinh_63134331.Models
+{
+    public class CHITIETHOADONDTO
+    {
+        public string IDSP { get; set; }
+        public string TENSP { get; set; }
+        public int SL { get; set; }
+        public string TSKT { get; set; }
+
+
+    }
+}
