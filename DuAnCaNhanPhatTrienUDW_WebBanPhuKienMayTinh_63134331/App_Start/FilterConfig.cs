9d34d4849f3bb7a00a7f24f0c2ab24bb353b7130
LBKhoa <lebaokhoa.200329@gmail.com>
2024-11-05 19:02:40 +0700

    update

---
+++ DuAnCaNhanPhatTrienUDW_WebBanPhuKienMayTinh_63134331/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DuAnCaNhanPhatTrienUDW_WebBanPhuKienMayTinh_63134331
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
